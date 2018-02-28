f1b4f60f1ed61fda435d6ee452a6c1273bf2be55
jisaiahgarrett <jisaiahgarrett@gmail.com>
2018-02-27 19:57:11 -0600

    Started start-basestation.sh

---
+++ startup/start-basestation.sh
@@ -0,0 +1,2 @@
+lxterminal --title="Client" -e 'bash -c "cd ~/Desktop/Mercury2018 && source venv/bin/activate && cd ksurct-robot-2018 && python Client.py;bash"'
+lxterminal --title="Stream" -e 'bash -c "cd ~/Desktop/Mercury2018 && cd ksurct-robot-2018/startup && ./stream.sh;bash"'
